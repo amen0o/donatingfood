82d097ebac2422a78e4c5a804e2bc0795393508a
Andrei Verciuc <averciuc@sdl.com>
2021-05-15 10:18:08 +0300

    Initial version

---
+++ FoodCarring/Entities/RepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Configuration;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Entities
+{
+    public class RepositoryContext : IdentityDbContext<User>
+    {
+        public RepositoryContext(DbContextOptions options)
+     : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        }
+
+    }
+}
