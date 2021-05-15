6d3d8bd475ad6927d95c1d951a65c2034b8b2fa3
Andrei Verciuc <averciuc@sdl.com>
2021-05-15 12:32:28 +0300

    Adds files back to ensure that .gitignore works

---
+++ FoodCarring/FoodCarring/Controller/AuthenticationController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using CompanyEmployees.ActionFilters;
+using Contracts;
+using Entities.DTOs;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FoodCaring.Controller
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IAuthenticationManager _authManager;
+        public AuthenticationController(IMapper mapper, UserManager<User> userManager, IAuthenticationManager authManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _authManager = authManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            //var user = _mapper.Map<User>(userForRegistration);
+            var user = new User()
+            {
+                FirstName = userForRegistration.FirstName,
+                LastName = userForRegistration.LastName,
+                UserName = userForRegistration.UserName,
+                PasswordHash = userForRegistration.Password,
+                Email = userForRegistration.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            if (!userForRegistration.Roles.Any())
+            {
+                await _userManager.AddToRoleAsync(user, "Manager");
+            }
+            else
+            {
+                await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            }
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Authenticate([FromBody] UserForAuthenticationDto user)
+        {
+            if (!await _authManager.ValidateUser(user))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { Token = await _authManager.CreateToken() });
+        }
+
+        [HttpPost("test")]
+        public IActionResult Test()
+        {
+            return Content("Reusit");
+        }
+        
+        [HttpPost("test2")]
+        [Authorize]
+        public IActionResult Test2()
+        {
+            return Content("Reusit2");
+        }
+    }
+}
---
+++ FoodCarring/FoodCarring/Extensions/ServiceExtensions.cs
@@ -0,0 +1,82 @@
+﻿using Entities;
+using Entities.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodCaring.Extensions
+{
+    public static  class ServiceExtensions 
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+           services.AddCors(options =>
+           {
+               options.AddPolicy("CorsPolicy", builder =>
+                   builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader());
+           });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+           services.AddDbContext<RepositoryContext>(opts =>
+               opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("FoodCaring")));
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<User>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 1;
+                o.User.RequireUniqueEmail = true;
+            });
+
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), builder.Services);
+            builder.AddEntityFrameworkStores<RepositoryContext>()
+                .AddDefaultTokenProviders();
+        }
+
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = Environment.GetEnvironmentVariable("SECRET");
+
+            services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
+                    ValidAudience = jwtSettings.GetSection("validAudience").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+    }
+}
