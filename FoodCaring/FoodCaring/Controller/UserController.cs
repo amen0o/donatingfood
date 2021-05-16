b230256c65483555dda7bdc5a18e5346bd856182
cristian_a_cioban <cristian.cioban@deleteagency.com>
2021-05-16 18:17:18 +0300

    added User getAll method

---
+++ FoodCaring/FoodCaring/Controller/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FoodCaring.Controller
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetAll()
+        {
+            var users = _userManager.Users.ToArray();
+
+            return Ok(users);
+        }
+    }
+}
