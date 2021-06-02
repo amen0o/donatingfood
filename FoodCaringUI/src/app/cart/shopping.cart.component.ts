cbd4f774a07e8bc15b5f891637de8e112cc6bc2d
amen0o <ciucky1@gmail.com>
2021-06-02 20:35:31 +0300

    adds shoppingcart ui

--- FoodCaringUI/src/app/cart/shopping.cart.component.ts
+++ FoodCaringUI/src/app/cart/shopping.cart.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { OrderService, AccountService} from '../_services';
+import { User } from '../_models';
 
 @Component({ templateUrl: 'shopping.cart.component.html' })
-export class ShoppingCartComponent { }
+export class ShoppingCartComponent implements OnInit {
+    order = null;
+    user = null;
+
+    constructor(
+        private orderService: OrderService,
+        private accountService: AccountService) {
+        
+    }
+
+    ngOnInit(): void {
+        this.user = this.accountService.userValue;
+        this.order = this.orderService.getOrder(this.user.Id);
+    }
+ }
